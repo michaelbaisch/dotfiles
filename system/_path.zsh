4d668368406ecce0d69e35bfc3181dcdeaf2023c
Michael Baisch <mail@michaelbais.ch>
2016-10-09 19:30:40 +0200

    Update system and zsh

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,4 @@
-# org old : export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/
-# org : export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-export PATH="$ZSH/bin:$PATH"
+# My old path: *rvm stuff*: $ZSH/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/git/bin:$HOME/.rvm/bin
+
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
